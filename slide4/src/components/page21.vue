08e3d731bdf02debaf53afb2b289663e0bbbea7a
Thien Dinh <thiendinh1608@gmail.com>
2026-01-17 19:10:18 +0700

    test

---
+++ slide4/src/components/page21.vue
@@ -0,0 +1,19 @@
+<template lang="">
+<div>
+<button v-on:click="this.ahihi = !ahihi" v-html="ahihi?
+'show' : 'hide'"> </button>
+</div>
+</template>
+<script>
+export default {
+data() {
+return {
+ahihi: true
+
+}
+
+}
+</script>
+<style lang="">
+
+</style>
