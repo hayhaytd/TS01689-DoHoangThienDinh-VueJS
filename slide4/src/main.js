b676682787b1e6273254cff747d34e5b2606adf1
Thien Dinh <thiendinh1608@gmail.com>
2026-01-20 22:45:19 +0700

    test

--- slide4/src/main.js
+++ slide4/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import tuoi from './components/tuoi.vue'
+import './style.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.component('tuoi', tuoi)
+app.mount('#app')
